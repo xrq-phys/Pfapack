b8d9a3dce86e82240a237bbeda8a3719fb3599b1
RuQing Xu <ruqing.xu@phys.s.u-tokyo.ac.jp>
2024-06-17 01:24:48 +0900

    0-placeholder in tester

--- fortran/TESTING/benchmark_ltl.c
+++ fortran/TESTING/benchmark_ltl.c
@@ -107,6 +107,8 @@ int main(const int argc, const char *argv[]) {
         (double)timediff_ns(time1, time2) / nexp / 1000000,
         npm);
   }
+  // Zero placeholder
+  fprintf(stdout, "%8d %18.14e %18.4e %18.4e %d\n", 0, 0, 1e-20, 1e-20, 0);
 
   free(A);
   free(W);
